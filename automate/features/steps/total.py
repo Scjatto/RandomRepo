2096c6befa149cc1d97e632e0ac3ddd6f2c33cc1
rebaca <scha99@gmail.com>
2019-08-23 18:18:58 +0530

    Merged into one step file

--- automate/features/steps/total.py
+++ automate/features/steps/total.py
@@ -1,12 +1,21 @@
 from behave import *
+from List import *
 
 @given("we have behave installed")
 def step_impl(context):
     pass
 
+@given('I have the list')
+def step_impl(context):
+        context.list = List()
+
 @when("we implement a test")
 def step_impl(context):
     assert True is not False
+
+@when('I spend Rupees {money:d}')
+def step_impl(context,money):
+        context.list_product = context.list.cost(money)
 
 @then("behave will test for us")
 def step_impl(context):
@@ -15,3 +24,8 @@ def step_impl(context):
 @then("celebrate")
 def step_impl(context):
     assert context.failed is False
+
+
+@then('I can buy {product}')
+def step_impl(context,product):
+    assert (context.list_product == product)
